83fd23d69016947581c38fb92e7385e2f27a7737
Junwon You <122416332+junwon0@users.noreply.github.com>
2023-12-18 11:45:33 +0900

    Update run.sh
    fix small coments

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Multi PHLP
 for name in NS USAir Power Celegans Router Yeast PB Ecoli 
 do
-  for i in 1
+  for i in 1 2 3 4 5 6 7 8 9 10
   do
     python main.py --data-name $name --seed $i --num-cpu 32 --onedim-PH True --multi-angle True --multiprocess True
   done
@@ -12,25 +12,25 @@ done
 # Multi PHLP (0dim)
 for name in NS USAir Power Celegans Router Yeast PB Ecoli 
 do
-  for i in 1
+  for i in 1 2 3 4 5 6 7 8 9 10
   do
     python main.py --data-name $name --seed $i --num-cpu 32 --onedim-PH False --multi-angle True --multiprocess True
   done
 done
 
-# Multi PHLP
+# PHLP
 for name in NS USAir Power Celegans Router Yeast PB Ecoli 
 do
-  for i in 1
+  for i in 1 2 3 4 5 6 7 8 9 10
   do
     python main.py --data-name $name --seed $i --num-cpu 32 --onedim-PH True --multi-angle False --multiprocess True
   done
 done
 
-# Multi PHLP (0dim)
+# PHLP (0dim)
 for name in NS USAir Power Celegans Router Yeast PB Ecoli 
 do
-  for i in 1
+  for i in 1 2 3 4 5 6 7 8 9 10
   do
     python main.py --data-name $name --seed $i --num-cpu 32 --onedim-PH False --multi-angle False --multiprocess True
   done
