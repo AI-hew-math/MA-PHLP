79fca211191434627f46c779b3e0791b36d3349b
Junwon You <122416332+junwon0@users.noreply.github.com>
2024-04-09 12:59:26 +0900

    Update run_PHSEAL.sh

--- PHSEAL/Python/run_PHSEAL.sh
+++ PHSEAL/Python/run_PHSEAL.sh
@@ -4,8 +4,7 @@ for name in NS USAir Power Celegans Router Yeast PB Ecoli
 do
   for i in 1 2 3 4 5 6 7 8 9 10
   do
-    python PHSEAL.py --data-name $name --seed $i --graph-feature None --multi-angle True #SEAL
-    python PHSEAL.py --data-name $name --seed $i --graph-feature True --multi-angle False #PHSEAL(0dim)
+    python PHSEAL.py --data-name $name --seed $i --hop 'auto' --graph-feature None --multi-angle True #SEAL
     python PHSEAL.py --data-name $name --seed $i --graph-feature True --multi-angle True  #MultiPHSEAL(0dim)
   done
 done
